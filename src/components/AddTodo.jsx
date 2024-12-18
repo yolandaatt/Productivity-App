c434b7b0e861c74b6cf63560fda4d5f91480e598
Meelstrm <melissa.ostrom@outlook.com>
2024-12-18 13:56:44 +0100

    Alla ändringar

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -2,25 +2,46 @@ import { useState } from "react"
 
 
 function AddTodo({addNewTodo}) {
-    const [value, setValue] = useState('')
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [timeEstimate, setTimeEstimate] = useState('')
+    const [category, setCategory] = useState('')
+    const [deadline, setDeadline] = useState('')
+    const [status, setStatus] = useState('Not started yet')
 
     const handleSubmit = e => {
         e.preventDefault()
-        addNewTodo(value)
+        addNewTodo({title, description, timeEstimate, category,deadline, status})
 
-        setValue('')
+        setTitle('')
+        setDescription('')
+        setTimeEstimate('')
+        setCategory('')
+        setDeadline('')
+        setStatus('Not started yet')
 
     }
 
     return(
         <> 
         <form className="addTodo" onSubmit={handleSubmit}>
-            <input type="text" className="todoInput" value={value} placeholder="Write a new task" onChange={(e) =>(setValue(e.target.value))}/>
+            <input type="text" className="todoInput" value={title} placeholder="Write your title" onChange={(e) =>(setTitle(e.target.value))}/>
+            <input type="text" className="todoInput" value={description} placeholder="Descripe your task" onChange={(e) => (setDescription(e.target.value))}/>
+            <input type="text" className="todoInput" value={timeEstimate} placeholder="Time Estimate" onChange={(e) =>(setTimeEstimate(e.target.value))}/>
+            <input type="text" className="todoInput" value={category} placeholder="Write the category of the task" onChange={(e) =>(setCategory(e.target.value))}/>
+            <input type="date" className="todoInput" value={deadline} onChange={(e) => setDeadline(e.target.value)} />
+            <select className="statusInput" value={status} onChange={(e) => setStatus(e.target.value)}>
+                 <option value="Not started yet">Not started yet</option> 
+                 <option value="In progress">In progress</option> 
+                 <option value="Completed">Completed</option>
+                 </select>
+
+            <br />
             <button type="submit" className="newTodoBtn">Add Task</button>
-        </form> 
+        </form>
         </>
 
     )
 }
 
-export default AddTodo        
+export default AddTodo                  
--- src/components/EditTodo.jsx
+++ src/components/EditTodo.jsx
@@ -1,11 +1,42 @@
+import { useState } from "react"
 
-function EditTodo() {
+
+function EditTodo({editTask, task}) {
+    const [title, setTitle] = useState(task.task.title)
+    const [description, setDescription] = useState(task.task.description)
+    const [timeEstimate, setTimeEstimate] = useState(task.task.timeEstimate)
+    const [category, setCategory] = useState(task.task.category)
+    const [deadline, setDeadline] = useState(task.task.deadline)
+    const [status, setStatus] = useState(task.task.status)
+
+    
+    const handleSubmit = e => {
+        e.preventDefault()
+        editTask({title, description, timeEstimate, category, deadline, status}, task.id)
+
+    }
+
     return(
-        <>
-        <h1>hej då</h1>
+        <> 
+        <form className="editTodo" onSubmit={handleSubmit}>
+              <input type="text" className="todoInput" value={title} placeholder="Write your title" onChange={(e) =>(setTitle(e.target.value))}/>
+            <input type="text" className="todoInput" value={description} placeholder="Descripe your task" onChange={(e) => (setDescription(e.target.value))}/>
+            <input type="text" className="todoInput" value={timeEstimate} placeholder="Time Estimate" onChange={(e) =>(setTimeEstimate(e.target.value))}/>
+            <input type="text" className="todoInput" value={category} placeholder="Write the category of the task" onChange={(e) =>(setCategory(e.target.value))}/>
+            <input type="date" className="todoInput" value={deadline} onChange={(e) => setDeadline(e.target.value)} />
+            <select className="statusInput" value={status} onChange={(e) => setStatus(e.target.value)}>
+                 <option value="Not started yet">Not started yet</option> 
+                 <option value="In progress">In progress</option> 
+                 <option value="Completed">Completed</option>
+                 </select>
+
+            <br />
+
+            <button type="submit" className="newTodoBtn">Update Task</button>
+        </form>
         </>
 
     )
 }
 
-export default EditTodo
+export default EditTodo                  
