c02468348570b2afca73a40b7c930ca4a79d0750
yolandaatt <yolandaatteyeh3@gmail.com>
2024-12-20 00:40:45 +0100

    senaste ändringar

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
-import react from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import Events from "./Components/Events";
-import EventForm from "./Components/EventForm";
-import StartPage from "./Components/StartPage";
+import { useState } from 'react'
+import Events from "./Components/Events"
+import "./App.css"
 
 
 function App () {
-    const [events, setEvents] = useState([]);
-    const addEvent = (newEvent) => {
-        setEvents((prevEvents) => [...prevEvents, newEvent])
-    }
-    
-    return (
-    
+  return (
     <div>
-        <Routes>
-        <Route path = "/" element = {<StartPage/>} />
-        <Route path = "/EventForm" element= {<EventForm addEvent={addEvent} />} />
-        <Route path = "/Events" element = {<Events events = {events}/>} /> 
-        </Routes>
-   </div> 
-   )
+      <Events/>
+    </div>
+  )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-     <App />
-    </Router>
+    <App />
   </StrictMode>,
 )
